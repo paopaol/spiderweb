09069587d479c939db6f2754275a49fe0942445c
paopaol <467847281@qq.com>
2022-12-24 21:54:44 +0800

    feat(core): add spiderweb::Thread class

---
+++ spiderweb/core/spiderweb_thread.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <thread>
+
+namespace spiderweb {
+
+/**
+ * @brief Thread Is EventLoop Thread.
+ * with this class, we can simple start a thread, that run an event loop.
+ * and, we can call `QueueTask()`  post a task, the task will called by the
+ * event loop in this thread
+ *
+ * @example
+ *  using TimerPtr = std::unique_ptr<spiderweb::Timer>;
+ *
+ *  spiderweb::Thread t;
+ *  TimerPtr          timer;
+ *
+ *  r.Start();
+ *  r.QueueTask([](){
+ *    timer = absl::make_unique<spiderweb::Timer>();
+ *
+ *    timer->SetInterval(100);
+ *    timer->SetSingalShot(true);
+ *
+ *    timer->Start();
+ *  });
+ *  :sleep(100000);
+ */
+class Thread {
+  class Private;
+
+  using Id = std::thread::id;
+
+ public:
+  Thread();
+
+  ~Thread();
+
+  /**
+   * @brief Thread The Loop Thread
+   */
+  void Start();
+
+  void Quit();
+
+  void QueueTask(std::function<void()> &&f);
+
+  bool IsRunning() const;
+
+  Id GetThreadId() const;
+
+ private:
+  std::shared_ptr<Private> d;
+};
+}  // namespace spiderweb
---
+++ spiderweb/core/spiderweb_thread_test.cc
@@ -0,0 +1,45 @@
+#include "spiderweb/core/spiderweb_thread.h"
+
+#include <chrono>
+#include <thread>
+
+#include "absl/memory/memory.h"
+#include "gtest/gtest.h"
+#include "spiderweb/core/spiderweb_timer.h"
+#include "spiderweb/core/spiderweb_waiter.h"
+
+class ThreadTest : public testing::Test {
+ public:
+  using TimerPtr = std::unique_ptr<spiderweb::Timer>;
+
+  spiderweb::Waiter<bool> waiter;
+  spiderweb::Thread       r;
+  TimerPtr                timer;
+  bool                    called = false;
+};
+
+TEST_F(ThreadTest, Run) {
+  EXPECT_FALSE(r.IsRunning());
+  r.Start();
+  EXPECT_TRUE(r.IsRunning());
+
+  r.QueueTask([&]() {
+    timer = absl::make_unique<spiderweb::Timer>();
+
+    timer->SetInterval(100);
+    timer->SetSingalShot(true);
+
+    spiderweb::Object::Connect(timer.get(), &spiderweb::Timer::timeout, timer.get(), [&]() {
+      called = true;
+      waiter.Notify(true);
+    });
+
+    timer->Start();
+  });
+
+  waiter.Wait();
+  EXPECT_TRUE(called);
+
+  r.Quit();
+  EXPECT_FALSE(r.IsRunning());
+}
