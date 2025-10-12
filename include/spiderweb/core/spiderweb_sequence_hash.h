2fd12e181213b2317cee2d747106838c7f8c1c84
jz <jz@paopaol.localdomain>
2025-10-12 21:38:11 +0800

    add spiderweb_sequence_hash

---
+++ include/spiderweb/core/spiderweb_sequence_hash.h
@@ -0,0 +1,536 @@
+#pragma once
+
+#include <cassert>
+#include <stdexcept>
+#include <unordered_map>
+
+namespace spiderweb {
+
+template <typename T>
+struct SequenceHashNode {
+  explicit SequenceHashNode(T&& v);
+
+  T                 v;
+  SequenceHashNode* p = nullptr;
+  SequenceHashNode* n = nullptr;
+};
+
+template <typename T>
+SequenceHashNode<T>::SequenceHashNode(T&& v) : v(std::forward<T>(v)) {
+}
+
+///////////////
+
+template <typename N, typename T, bool IsConst = false>
+struct SequenceHashIterator {
+  using element_type = typename std::conditional<IsConst, const N, N>::type;
+  using value_type = typename std::conditional<IsConst, const T, T>::type;
+  using pointer = typename std::conditional<IsConst, const T*, T*>::type;
+  using reference = typename std::conditional<IsConst, const T&, T&>::type;
+  using difference_type = std::ptrdiff_t;
+
+  explicit SequenceHashIterator(element_type* p);
+
+  SequenceHashIterator& operator=(const SequenceHashIterator& o);
+
+  bool operator==(const SequenceHashIterator& o) const;
+
+  bool operator!=(const SequenceHashIterator& o) const;
+
+  SequenceHashIterator& operator++();
+
+  SequenceHashIterator operator++(int);
+
+  SequenceHashIterator& operator--();
+
+  SequenceHashIterator operator--(int);
+
+  reference operator*();
+
+  pointer operator->();
+
+  pointer operator->() const;
+
+ private:
+  element_type* ptr = nullptr;
+
+  template <typename K, typename V>
+  friend class SequenceHash;
+};
+
+/**
+ * @brief
+ *
+ * SequenceHash is a container data structure that combines hash
+ *
+ * table and linear ordering. It can be thought of as a non-cyclic
+ *
+ * doubly linked list, meaning that elements are sorted in the order
+ *
+ * of insertion. However, unlike traditional linked lists, SequenceHash
+ *
+ * requires a unique key for each element, allowing for an internal
+ *
+ * hash table (for fast lookups, modifications, and deletions). When
+ *
+ * traversing a SequenceHash, elements are output in the order they
+ *
+ * were inserted.
+ *
+ * Note: Do not use sorting; sorting behavior is currently undefined.
+ */
+template <typename K, typename T>
+class SequenceHash {
+ public:
+  using key_type = typename std::decay<K>::type;
+  using node_type = SequenceHashNode<T>;
+
+  using Iterator = SequenceHashIterator<node_type, T>;
+
+  using ConstIterator = SequenceHashIterator<node_type, T, true>;
+
+  SequenceHash();
+
+  SequenceHash(SequenceHash&&) noexcept;
+
+  SequenceHash& operator=(SequenceHash&&) noexcept;
+
+  SequenceHash(const SequenceHash&) = delete;
+
+  SequenceHash& operator=(const SequenceHash&) = delete;
+
+  ~SequenceHash();
+
+  template <typename U>
+  bool PushBack(key_type&& key, U&& v);
+
+  template <typename U>
+  bool PushAfter(const key_type& at, key_type&& key, U&& v);
+
+  template <typename U>
+  bool PushFront(key_type&& key, U&& v);
+
+  template <typename U>
+  bool PushBefore(const key_type& at, key_type&& key, U&& v);
+
+  void Remove(const key_type& key);
+
+  void PopFront();
+
+  void PopBack();
+
+  void Clear();
+
+  std::size_t Size() const;
+
+  bool Empty() const;
+
+  ConstIterator Find(const key_type& key) const;
+
+  Iterator Find(const key_type& key);
+
+  Iterator Forward(const key_type& current);
+
+  ConstIterator Forward(const key_type& current) const;
+
+  Iterator Backward(const key_type& current);
+
+  ConstIterator Backward(const key_type& current) const;
+
+  Iterator Front();
+
+  ConstIterator Front() const;
+
+  Iterator Back();
+
+  ConstIterator Back() const;
+
+  ConstIterator begin() const;
+
+  ConstIterator end() const;
+
+  Iterator begin();
+
+  Iterator end();
+
+ private:
+  void PushNode(node_type* node);
+
+  void PushNode(node_type* before, node_type* node);
+
+  template <typename M, typename U>
+  node_type* InsertHash(M&& key, U&& v);
+
+  std::unordered_map<key_type, node_type*> nodes_;
+  node_type*                               tail_ = nullptr;
+  node_type*                               head_ = nullptr;
+};
+
+template <typename N, typename T, bool IsConst>
+SequenceHashIterator<N, T, IsConst>::SequenceHashIterator(element_type* p) : ptr(p) {
+}
+
+template <typename N, typename T, bool IsConst>
+SequenceHashIterator<N, T, IsConst>& SequenceHashIterator<N, T, IsConst>::operator=(
+    const SequenceHashIterator& o) {
+  if (this == &o) {
+    return *this;
+  }
+  ptr = o.ptr;
+  return *this;
+}
+
+template <typename N, typename T, bool IsConst>
+bool SequenceHashIterator<N, T, IsConst>::operator==(const SequenceHashIterator& o) const {
+  return this->ptr == o.ptr;
+}
+
+template <typename N, typename T, bool IsConst>
+bool SequenceHashIterator<N, T, IsConst>::operator!=(const SequenceHashIterator& o) const {
+  return this->ptr != o.ptr;
+}
+
+template <typename N, typename T, bool IsConst>
+SequenceHashIterator<N, T, IsConst>& SequenceHashIterator<N, T, IsConst>::operator++() {
+  if (!ptr) {
+    throw std::runtime_error("out of range");
+  }
+
+  this->ptr = this->ptr->n;
+  return *this;
+}
+
+template <typename N, typename T, bool IsConst>
+SequenceHashIterator<N, T, IsConst> SequenceHashIterator<N, T, IsConst>::operator++(int) {
+  auto tmp = *this;
+  ++(*this);
+  return tmp;
+}
+
+template <typename N, typename T, bool IsConst>
+SequenceHashIterator<N, T, IsConst>& SequenceHashIterator<N, T, IsConst>::operator--() {
+  if (!ptr) {
+    throw std::runtime_error("out of range");
+  }
+
+  this->ptr = this->ptr->p;
+  return *this;
+}
+
+template <typename N, typename T, bool IsConst>
+SequenceHashIterator<N, T, IsConst> SequenceHashIterator<N, T, IsConst>::operator--(int) {
+  auto tmp = *this;
+  --(*this);
+  return tmp;
+}
+
+template <typename N, typename T, bool IsConst>
+typename SequenceHashIterator<N, T, IsConst>::reference
+SequenceHashIterator<N, T, IsConst>::operator*() {
+  if (!ptr) {
+    throw std::runtime_error("out of range");
+  }
+
+  return this->ptr->v;
+}
+
+template <typename N, typename T, bool IsConst>
+typename SequenceHashIterator<N, T, IsConst>::pointer
+SequenceHashIterator<N, T, IsConst>::operator->() {
+  if (!ptr) {
+    throw std::runtime_error("out of range");
+  }
+  return &this->ptr->v;
+}
+
+template <typename N, typename T, bool IsConst>
+typename SequenceHashIterator<N, T, IsConst>::pointer
+SequenceHashIterator<N, T, IsConst>::operator->() const {
+  if (!ptr) {
+    throw std::runtime_error("out of range");
+  }
+  return &this->ptr->v;
+}
+
+//////////////////////////////////
+template <typename T, typename KeyFun>
+inline SequenceHash<T, KeyFun>::SequenceHash() = default;
+
+template <typename K, typename T>
+SequenceHash<K, T>::SequenceHash(SequenceHash&& other) noexcept {
+  this->nodes_ = std::move(other.nodes_);
+  this->head_ = other.head_;
+  this->tail_ = other.tail_;
+
+  other.head_ = nullptr;
+  other.tail_ = nullptr;
+}
+
+template <typename K, typename T>
+SequenceHash<K, T>& SequenceHash<K, T>::operator=(SequenceHash&& other) noexcept {
+  if (this != &other) {
+    Clear();
+
+    this->nodes_ = std::move(other.nodes_);
+    this->head_ = other.head_;
+    this->tail_ = other.tail_;
+
+    other.head_ = nullptr;
+    other.tail_ = nullptr;
+  }
+
+  return *this;
+}
+
+template <typename T, typename KeyFun>
+inline SequenceHash<T, KeyFun>::~SequenceHash() {
+  Clear();
+}
+
+template <typename K, typename T>
+template <typename U>
+bool SequenceHash<K, T>::PushBack(key_type&& key, U&& v) {
+  auto* node = InsertHash(std::forward<key_type>(key), std::forward<U>(v));
+  if (node) {
+    PushNode(node);
+  }
+  return !!node;
+}
+
+template <typename K, typename T>
+template <typename U>
+bool SequenceHash<K, T>::PushAfter(const key_type& at, key_type&& key, U&& v) {
+  auto before = Find(at);
+  if (before == end()) {
+    return false;
+  }
+
+  auto* node = InsertHash(std::forward<key_type>(key), std::forward<U>(v));
+  if (node) {
+    PushNode(before.ptr, node);
+    return true;
+  }
+  return false;
+}
+
+template <typename K, typename T>
+template <typename U>
+bool SequenceHash<K, T>::PushFront(key_type&& key, U&& v) {
+  //   head_
+  // n head_
+  auto* node = InsertHash(std::forward<key_type>(key), std::forward<U>(v));
+  if (!node) {
+    return false;
+  }
+
+  if (head_) {
+    PushNode(node, head_);
+    head_ = node;
+  } else {
+    PushNode(node);
+  }
+
+  return true;
+}
+
+template <typename K, typename T>
+template <typename U>
+bool SequenceHash<K, T>::PushBefore(const key_type& at, key_type&& key, U&& v) {
+  //   before
+  // n before
+  auto before = Find(at);
+  if (before == end()) {
+    return false;
+  }
+
+  auto* node = InsertHash(std::forward<key_type>(key), std::forward<U>(v));
+  if (!node) {
+    return false;
+  }
+
+  if (before.ptr->p) {
+    PushNode(before.ptr->p, node);
+  } else {
+    PushNode(node, head_);
+    head_ = node;
+  }
+
+  return true;
+}
+
+template <typename K, typename T>
+void SequenceHash<K, T>::Remove(const key_type& key) {
+  auto it = Find(key);
+  if (it == end()) {
+    return;
+  }
+
+  // p current n
+  // p         n
+  auto* current = it.ptr;
+  if (current->p) {
+    current->p->n = current->n;
+  }
+  if (current->n) {
+    current->n->p = current->p;
+  }
+
+  nodes_.erase(key);
+  delete current;
+}
+
+template <typename K, typename T>
+void SequenceHash<K, T>::PushNode(node_type* node) {
+  if (head_) {
+    node->p = tail_;
+    node->n = tail_->n;
+
+    tail_->n = node;
+  } else {
+    assert(!node->p && !node->n);
+
+    head_ = node;
+  }
+  tail_ = node;
+}
+
+template <typename K, typename T>
+void SequenceHash<K, T>::PushNode(node_type* before, node_type* node) {
+  assert(before && node);
+
+  // before   next
+  // before n next
+
+  auto* next = before->n;
+  node->n = next;
+  if (next) {
+    next->p = node;
+  }
+
+  before->n = node;
+  node->p = before;
+}
+
+template <typename K, typename T>
+template <typename M, typename U>
+typename SequenceHash<K, T>::node_type* SequenceHash<K, T>::InsertHash(M&& key, U&& v) {
+  node_type* node = nullptr;
+
+  auto result = nodes_.insert({std::forward<M>(key), nullptr});
+  if (!result.second) {
+    return nullptr;
+  }
+
+  node = new node_type(std::forward<U>(v));
+  result.first->second = node;
+  return node;
+}
+
+template <typename T, typename KeyFun>
+void SequenceHash<T, KeyFun>::Clear() {
+  nodes_.clear();
+
+  auto* current = tail_;
+
+  while (current) {
+    auto* p = current->p;
+    delete current;
+
+    if (p) {
+      p->n = nullptr;
+    }
+    current = p;
+  }
+  head_ = tail_ = nullptr;
+}
+
+template <typename T, typename KeyFun>
+std::size_t SequenceHash<T, KeyFun>::Size() const {
+  return nodes_.size();
+}
+
+template <typename T, typename KeyFun>
+bool SequenceHash<T, KeyFun>::Empty() const {
+  return nodes_.empty();
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::ConstIterator SequenceHash<K, T>::Find(const key_type& key) const {
+  auto it = nodes_.find(key);
+  return it == nodes_.end() ? ConstIterator(nullptr) : ConstIterator(it->second);
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::Iterator SequenceHash<K, T>::Find(const key_type& key) {
+  auto it = nodes_.find(key);
+  return it == nodes_.end() ? Iterator(nullptr) : Iterator(it->second);
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::Iterator SequenceHash<K, T>::Forward(const key_type& current) {
+  auto it = Find(current);
+  return it != end() ? ++it : end();
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::ConstIterator SequenceHash<K, T>::Forward(
+    const key_type& current) const {
+  auto it = Find(current);
+  return it != end() ? ++it : end();
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::Iterator SequenceHash<K, T>::Backward(const key_type& current) {
+  auto it = Find(current);
+  return it != end() ? --it : end();
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::ConstIterator SequenceHash<K, T>::Backward(
+    const key_type& current) const {
+  auto it = Find(current);
+  return --it;
+  return it != end() ? --it : end();
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::Iterator SequenceHash<K, T>::Front() {
+  return Iterator(head_);
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::ConstIterator SequenceHash<K, T>::Front() const {
+  return ConstIterator(head_);
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::Iterator SequenceHash<K, T>::Back() {
+  return Iterator(tail_);
+}
+
+template <typename K, typename T>
+typename SequenceHash<K, T>::ConstIterator SequenceHash<K, T>::Back() const {
+  return ConstIterator(tail_);
+}
+
+template <typename T, typename KeyFun>
+typename SequenceHash<T, KeyFun>::ConstIterator SequenceHash<T, KeyFun>::begin() const {
+  return ConstIterator(head_);
+}
+
+template <typename T, typename KeyFun>
+typename SequenceHash<T, KeyFun>::ConstIterator SequenceHash<T, KeyFun>::end() const {
+  return ConstIterator(nullptr);
+}
+
+template <typename T, typename KeyFun>
+typename SequenceHash<T, KeyFun>::Iterator SequenceHash<T, KeyFun>::begin() {
+  return Iterator(head_);
+}
+
+template <typename T, typename KeyFun>
+typename SequenceHash<T, KeyFun>::Iterator SequenceHash<T, KeyFun>::end() {
+  return Iterator(nullptr);
+}
+
+}  // namespace spiderweb
---
+++ spiderweb/core/spiderweb_sequence_hash_test.cc
@@ -0,0 +1,195 @@
+#include "spiderweb/core/spiderweb_sequence_hash.h"
+
+#include "gtest/gtest.h"
+
+struct User {
+  std::string id;
+  int         age;
+};
+
+namespace spiderweb {
+class SequenceHashTest : public testing::Test {
+ public:
+  void SetUp() override {
+    users.PushBack("123", User{"123", 19});
+    users.PushBack("abc", User{"abc", 23});
+  }
+
+  SequenceHash<std::string, User> users;
+};
+
+TEST_F(SequenceHashTest, Construct) {
+  SequenceHash<std::string, User> v;
+  EXPECT_TRUE(v.Empty());
+}
+
+TEST_F(SequenceHashTest, MoveConstruct) {
+  SequenceHash<std::string, User> v(std::move(users));
+
+  EXPECT_EQ(v.Size(), 2);
+  EXPECT_TRUE(users.Empty());
+}
+
+TEST_F(SequenceHashTest, MoveAssgin) {
+  SequenceHash<std::string, User> v;
+
+  v.PushBack("front", User{"front", 123});
+
+  v = std::move(users);
+  EXPECT_EQ(v.Size(), 2);
+  EXPECT_TRUE(users.Empty());
+}
+
+TEST_F(SequenceHashTest, PushBack) {
+  EXPECT_EQ(users.Size(), 2);
+  EXPECT_FALSE(users.Empty());
+}
+
+TEST_F(SequenceHashTest, PushFront) {
+  EXPECT_TRUE(users.PushFront("front", User{"front", 123}));
+
+  std::vector<std::string> result{"front", "123", "abc"};
+  int                      i = 0;
+  for (auto it = users.begin(); it != users.end(); ++it) {
+    EXPECT_EQ(it->id, result[i]);
+    ++i;
+  }
+}
+
+TEST_F(SequenceHashTest, PushFrontwhenEmpty) {
+  SequenceHash<std::string, User> v;
+  EXPECT_TRUE(v.PushFront("front", User{"front", 123}));
+}
+
+TEST_F(SequenceHashTest, PushFailed) {
+  EXPECT_FALSE(users.PushBack("123", User{}));
+}
+
+TEST_F(SequenceHashTest, PushAfter) {
+  EXPECT_TRUE(users.PushAfter("123", "x", User{"x", 33}));
+
+  std::vector<std::string> result{"123", "x", "abc"};
+  int                      i = 0;
+  for (auto it = users.begin(); it != users.end(); ++it) {
+    EXPECT_EQ(it->id, result[i]);
+    ++i;
+  }
+}
+
+TEST_F(SequenceHashTest, PushBefore) {
+  EXPECT_TRUE(users.PushBefore("123", "before", User{"before", 123}));
+
+  std::vector<std::string> result{"before", "123", "abc"};
+  int                      i = 0;
+  for (auto it = users.begin(); it != users.end(); ++it) {
+    EXPECT_EQ(it->id, result[i]) << i;
+    ++i;
+  }
+}
+
+TEST_F(SequenceHashTest, Remove) {
+  users.Remove("123");
+
+  EXPECT_EQ(users.Size(), 1);
+  EXPECT_EQ(users.Find("123"), users.end());
+  EXPECT_EQ(users.Find("abc")->id, "abc");
+}
+
+TEST_F(SequenceHashTest, Clear) {
+  users.Clear();
+  EXPECT_TRUE(users.Empty());
+}
+
+TEST_F(SequenceHashTest, Begin) {
+  auto beg = users.begin();
+  EXPECT_EQ(beg->id, "123");
+}
+
+TEST_F(SequenceHashTest, For) {
+  std::vector<std::string> result{"123", "abc"};
+  int                      i = 0;
+  for (auto it = users.begin(); it != users.end(); ++it) {
+    EXPECT_EQ(it->id, result[i]);
+    ++i;
+  }
+}
+
+TEST_F(SequenceHashTest, ForRange) {
+  std::vector<std::string> result{"123", "abc"};
+  int                      i = 0;
+  for (auto& it : users) {
+    EXPECT_EQ(it.id, result[i]);
+    ++i;
+  }
+}
+
+TEST_F(SequenceHashTest, ConstForRange) {
+  std::vector<std::string> result{"123", "abc"};
+  int                      i = 0;
+  for (const auto& it : users) {
+    EXPECT_EQ(it.id, result[i]);
+    ++i;
+  }
+}
+
+TEST_F(SequenceHashTest, Front) {
+  EXPECT_EQ(users.Front()->id, "123");
+  const auto it = users.Front();
+  EXPECT_EQ(it->id, "123");
+}
+
+TEST_F(SequenceHashTest, Back) {
+  EXPECT_EQ(users.Back()->id, "abc");
+  const auto it = users.Back();
+  EXPECT_EQ(it->id, "abc");
+
+  const SequenceHash<std::string, User> us;
+}
+
+TEST_F(SequenceHashTest, Find) {
+  EXPECT_NE(users.Find("123"), users.end());
+  EXPECT_NE(users.Find("abc"), users.end());
+  EXPECT_EQ(users.Find(""), users.end());
+
+  EXPECT_EQ(users.Find("123")->id, "123");
+}
+
+TEST_F(SequenceHashTest, ForRangePrint) {
+  for (auto& it : users) {
+    printf("id: %s age: %d\n", it.id.c_str(), it.age);
+  }
+}
+
+TEST_F(SequenceHashTest, Forward) {
+  auto it = users.Forward("123");
+  EXPECT_NE(it, users.end());
+  EXPECT_EQ(it->id, "abc");
+}
+
+// 123 abc
+TEST_F(SequenceHashTest, ForwardNotFound) {
+  auto it = users.Forward("bad-key");
+  EXPECT_EQ(it, users.end());
+}
+
+TEST_F(SequenceHashTest, Backward) {
+  auto it = users.Backward("abc");
+  EXPECT_NE(it, users.end());
+  EXPECT_EQ(it->id, "123");
+}
+
+TEST_F(SequenceHashTest, BackwardNotFound) {
+  auto it = users.Backward("bad-key");
+  EXPECT_EQ(it, users.end());
+}
+
+TEST_F(SequenceHashTest, BackRangePrint) {
+  auto current = users.Back();
+
+  while (current != users.end()) {
+    printf("id: %s age: %d\n", current->id.c_str(), current->age);
+    current--;
+  }
+}
+
+}  // namespace spiderweb
